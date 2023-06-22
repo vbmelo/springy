3b8918cab3f6ecc44c14561ec4f75bec78ded8ca
Victor Melo <vbmelo@ua.pt>
2023-06-22 16:52:01 +0100

    starting the frontend and backend projects

---
+++ backend/src/test/java/tqs/backend/TestBackendApplication.java
@@ -0,0 +1,22 @@
+package tqs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MongoDBContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestBackendApplication {
+
+	@Bean
+	@ServiceConnection
+	MongoDBContainer mongoDbContainer() {
+		return new MongoDBContainer("mongo:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(BackendApplication::main).with(TestBackendApplication.class).run(args);
+	}
+
+}
